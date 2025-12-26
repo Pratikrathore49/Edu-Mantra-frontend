c6d69aeff4a06ff7ece1235f9e07b42ca60878a1
Pratik Rathore <jipratik49@gmail.com>
2025-12-26 16:27:46 +0530

    fix: Update baseURL in axiosInstance to correct backend API endpoint

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,6 +1,6 @@
    import axios from "axios";
    const axiosInstance = axios.create({
-        baseURL:"https://edu-mantra-backend-1.onrender.com/api/",
+        baseURL:"https://edu-mantra-backend.onrender.com/api/",
         
         withCredentials: true,
         
